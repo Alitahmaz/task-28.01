26c2362f1a7538001250c7202f99eac5436753d3
Alitahmaz <alirt@code.edu.az>
2023-01-28 21:07:24 +0300

    first commit

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,189 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //task1
+            //    Console.Write("c ededi daxil et:");
+            //    int c = int.Parse(Console.ReadLine());
+            //    int n = int.Parse(Console.ReadLine());
+            //    int[] a = new int[n];
+            //    for (int i = 0; i < a.Length;i++)
+            //    {
+            //        Console.Write($"a[{i}]=");
+            //        a[i] = int.Parse(Console.ReadLine());
+            //    }
+            //    for (int i = 0; i < a.Length; i++)
+            //    {
+            //        if (a[i] < c)
+            //        {
+            //            Console.WriteLine($"{a[i]} < {c}");
+            //        }
+            //    }
+
+            //}
+            //task2
+            //Console.Write("c ededi daxil et:");
+            //int c = int.Parse(Console.ReadLine());
+            //int n = int.Parse(Console.ReadLine());
+            //int[] a = new int[n];
+            //int sum = 0;
+            //int count = 0;
+            //for (int i = 0; i < a.Length; i++)
+            //{
+            //    Console.Write($"a[{i}]=");
+            //    a[i] = int.Parse(Console.ReadLine());
+            //}
+            //for(int i = 0; i < a.Length; i++)
+            //{
+            //    if (a[i] < c)
+            //    {
+            //        sum += a[i];
+            //        count++;
+            //    }
+            //}
+            //Console.WriteLine((double)sum/count);
+            //task3
+            //Console.WriteLine("c ededi daxil et: ");
+            //int c = int.Parse(Console.ReadLine());
+            //int n = int.Parse(Console.ReadLine());
+            //int[] a = new int[n];
+            //int hasil = 1;
+            //int count = 0;
+            //for (int i = 0; i < a.Length; i++)
+            //{
+            //    Console.WriteLine($"a[{i}]=");
+            //    a[i] = int.Parse(Console.ReadLine());
+
+            //}
+            //for (int i = 0; i < a.Length; i++)
+            //{
+            //    if (a[i] > c)
+            //    {
+            //        hasil *= a[i];
+
+            //        count++;
+            //    }
+
+            //}
+            //Console.WriteLine(Math.Pow(hasil, 1.0 / count));
+            //task4
+            //Console.WriteLine("c ededi daxil et: ");
+            //int c = int.Parse(Console.ReadLine());
+            //int n = int.Parse(Console.ReadLine());
+            //int[] a = new int[n];
+            //int[] b = new int[n];
+            //Console.Write("c=");
+
+            //for (int i = 0; i < a.Length; i++)
+            //{
+            //    Console.WriteLine($"a[{i}]=");
+            //    a[i] = int.Parse(Console.ReadLine());
+            //}
+            //for (int i = 0; i < a.Length; i++)
+            //{
+            //    if (a[i] > 0)
+            //    {
+            //        b[i] = a[i];
+            //        Console.Write($"{b[i]}  ");
+            //    }
+            //}
+            //int temp = 0;
+            //for(int i=0; i < b.Length-1; i++)
+            //{
+            //    for (int j = i; j < b.Length; j++)
+
+            //    {
+            //        temp = b[i];
+            //        b[i] = b[j];
+            //        b[j] = temp;
+            //    }
+            //    Console.WriteLine();
+            //    foreach (var item in b)
+            //    {
+            //        Console.Write($"{item}");
+
+            //    }
+            //}
+            //task5
+            //Console.WriteLine("c ededi daxil et: ");
+            //int c = int.Parse(Console.ReadLine());
+            //int n = int.Parse(Console.ReadLine());
+            //int[] a = new int[n];
+            //int[] b = new int[n];
+            //int temp;
+            //for (int i = 0; i < a.Length; i++)
+            //{
+            //    Console.WriteLine($"a[{i}]=");
+            //    a[i] = int.Parse(Console.ReadLine());
+            //}
+            //for (int i = 0; i < a.Length; i++)
+            //{
+            //    if (a[i] < c)
+            //    {
+            //        b[i] = a[i];
+            //    }
+            //}
+            //for (int i = 0; i < a.Length-1; i++)
+            //{
+            //    for (int j = 0; j < a.Length; j++)
+            //    {
+            //        if (b[i] > b[j])
+            //        {
+            //            temp = b[i];
+            //            b[i] = b[j];
+            //            b[j] = temp;
+            //        }
+            //    }
+
+            //}
+            //Console.WriteLine(b[b.Length - 1]);
+            //task6
+            //int a = int.Parse(Console.ReadLine());
+            //int b = int.Parse(Console.ReadLine());
+            //int[] c = new int[a];
+            //int[] y = new int[b];
+            //for (int i = 0; i < c.Length; i++)
+            //{
+            //    Console.WriteLine($"a[{i}]");
+            //    y[i] = int.Parse(Console.ReadLine());
+            //}
+            //for (int i = 0; i < c.Length; i++)
+            //{
+            //    c[i] = y[i] * y[i];
+            //    Console.WriteLine($"{c[i]}  ");
+            //}
+            //task7
+            //int n = int.Parse(Console.ReadLine());
+            //int[] a = new int[n];
+            //int temp;
+            //for (int i = 0; i < a.Length; i++)
+
+            //{
+            //    a[i] = int.Parse(Console.ReadLine());
+            //    Console.WriteLine($"a[{i}]");
+            //}
+            //for (int i = 0; i < a.Length - 1; i++)
+            //{
+            //    for(int j = 0; j < a.Length; j++)
+            //    {
+            //        if (a[i] > a[j])
+            //        {
+            //            temp = a[i];
+            //            a[i] = a[j];
+            //            a[j] = temp;
+
+            //        }
+
+
+            //    }
+
+            //}
+            //int sum = a[0] + a[a.Length - 1];
+            //Console.WriteLine(sum/2);
+            //int total = a[0] * a[a.Length - 1];
+            //Console.WriteLine(Math.Sqrt(total)); 
+        }
+    }
+}
